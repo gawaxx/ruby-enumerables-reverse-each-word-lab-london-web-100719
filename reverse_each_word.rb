884a11c7a6749e0916f960d8934e63cd74c0c06a
jules blanc <jules.blanc@unccub.com>
2019-10-03 22:53:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
-  array = string.split('')
+  array = sentence.split(' ')
   array.collect {|word| word.reverse!}
-  return array.join('')
+  string = array.join(' ')
+  return string
 end
